66fd34127b6f9cc1c335c8f38168e86a67e76aef
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-09-10 21:37:16 +1000

    0.2.0支持摇一摇

--- hamood.js
+++ hamood.js
@@ -1,5 +1,72 @@
 (() => {
-    const HamoodDiv = `<div id="hamood" style="text-align:center;position:absolute;z-index:999;top:0;bottom:0;left:0;right:0;background-color:#fff;width:auto;height:auto;opacity:1;transition:opacity .5s"><img id="hamoodSplash" style="position:absolute;top:0;bottom:0;left:0;right:0;width:100%;height:100%;object-fit:cover;opacity:0;transition:opacity .5s" border="0"> <svg id="hamoodLoading" style="display:none;width:32px;height:32px;position:absolute;bottom:50vh;margin:auto;left:0;right:0" xmlns="http://www.w3.org/2000/svg" version="1" viewBox="0 0 16 16" width="16" height="16"><style>@keyframes rotate{0%{transform:rotate(0)}to{transform:rotate(360deg)}}@keyframes fillunfill{0%{stroke-dashoffset:32.3}50%{stroke-dashoffset:0}to{stroke-dashoffset:-31.9}}@keyframes rot{0%{transform:rotate(0)}to{transform:rotate(-360deg)}}@keyframes colors{0%,to{stroke:#000}}</style><g style="animation-duration:1.568s;animation-iteration-count:infinite;animation-name:rotate;animation-timing-function:linear;transform-origin:50% 50%;width:16px;height:16px"><path fill="none" d="M8 1.125A6.875 6.875 0 1 1 1.125 8" stroke-width="1.5" stroke-linecap="round" style="animation-duration:1333ms,5332ms,5332ms;animation-fill-mode:forwards;animation-iteration-count:infinite,infinite,infinite;animation-name:fillunfill,rot,colors;animation-play-state:running,running,running;animation-timing-function:cubic-bezier(.4,0,.2,1),steps(4),linear;transform-origin:50% 50%" stroke-dasharray="32.4" stroke-dashoffset="32.4"/></g></svg><h4 id="hamoodText" style="position:absolute;bottom:10vh;white-space:nowrap;left:50%;transform:translate(-50%,0);background-color:rgba(0,0,0,.4);color:#fff;border-radius:15px;padding:2vh"></h4><p id="hamoodSkip" style="position:absolute;right:32px;top:32px;background-color:rgba(0,0,0,.5);color:#fff;padding:1vh;font-size:1rem;border-radius:4px">跳过</p></div>`;
+    const HamoodDiv = `<div id="hamood"
+    style="text-align: center;position:absolute;z-index:999;top:0px;bottom:0px;left:0px;right:0px;background-color: white;width:auto;height:auto;opacity: 1;transition: opacity 0.5s;">
+    <img id="hamoodSplash"
+        style="position: absolute;top:0px;bottom:0px;left:0px;right:0px;width:100%;height:100%;object-fit:cover;opacity:0;transition: opacity 0.5s;"
+        border="0">
+    <svg id="hamoodLoading"
+        style="display:none;width:32px;height:32px;position:absolute;bottom:50vh;margin:auto;left:0;right:0;"
+        xmlns="http://www.w3.org/2000/svg" version="1" viewBox="0 0 16 16" width="16" height="16">
+        <style>
+            @keyframes rotate {
+                0% {
+                    transform: rotate(0deg)
+                }
+
+                to {
+                    transform: rotate(360deg)
+                }
+            }
+
+            @keyframes fillunfill {
+                0% {
+                    stroke-dashoffset: 32.3
+                }
+
+                50% {
+                    stroke-dashoffset: 0
+                }
+
+                to {
+                    stroke-dashoffset: -31.9
+                }
+            }
+
+            @keyframes rot {
+                0% {
+                    transform: rotate(0deg)
+                }
+
+                to {
+                    transform: rotate(-360deg)
+                }
+            }
+
+            @keyframes colors {
+
+                0%,
+                to {
+                    stroke: #000000
+                }
+            }
+        </style>
+        <g
+            style="animation-duration:1568.63ms;animation-iteration-count:infinite;animation-name:rotate;animation-timing-function:linear;transform-origin:50% 50%;width:16px;height:16px">
+            <path fill="none" d="M8 1.125A6.875 6.875 0 1 1 1.125 8" stroke-width="1.5" stroke-linecap="round"
+                style="animation-duration:1333ms,5332ms,5332ms;animation-fill-mode:forwards;animation-iteration-count:infinite,infinite,infinite;animation-name:fillunfill,rot,colors;animation-play-state:running,running,running;animation-timing-function:cubic-bezier(.4,0,.2,1),steps(4),linear;transform-origin:50% 50%"
+                stroke-dasharray="32.4" stroke-dashoffset="32.4" />
+        </g>
+    </svg>
+    <div id="hamoodShake"
+        style="display:none;position:absolute;bottom:10vh;left: 50%; transform: translate(-50%,0); background-color:rgba(0, 255, 0, 0.4);border-radius: 15px;transition: all 0.1s;">
+    </div>
+    <H4 id="hamoodText"
+        style="position:absolute;bottom:10vh;white-space: nowrap;left: 50%; transform: translate(-50%,0); background-color:rgba(0,0,0,0.4);color:white;border-radius: 15px;padding:2vh;">
+    </H4>
+    <p id="hamoodSkip"
+        style="position:absolute;right:32px;top:32px;background-color:rgba(0,0,0,0.5);color:white;padding:1vh;font-size:1rem;border-radius: 4px;">
+        跳过</p>
+</div>`;
     const ratio = window.innerWidth / window.innerHeight;
     let storageData = localStorage.hamood ? JSON.parse(localStorage.hamood) : {};
     const updateStorage = () => {
@@ -60,7 +127,7 @@
                 console.log("Not new open page, skip Hamood.");
                 skip = true;
             }
-            console.log("Hamood Version:0.1.6");
+            console.log("Hamood Version:0.2.0");
             let startTime = Date.now();
             if (!storageData.cachedSplash || !storageData.cachedUrl == config.data) {
                 let splash = await (await fetch(config.data).catch(() => { console.error("Failed to load Hamood data."); })).json().catch(() => { console.error("Failed to load Hamood data."); });
@@ -152,7 +219,43 @@
                         waitToExit(selected.showTime);
                     };
                     let textDiv = document.getElementById("hamoodText");
+                    let shakeDiv = document.getElementById("hamoodShake");
                     textDiv.innerText = selected.text;
+                    window.addEventListener('deviceorientation', handleOrientation);
+                    let enableShake = false;
+                    let initialOrientation = {};
+                    let fullWidth = 0;
+                    function handleOrientation(event) {
+                        console.log(event);
+                        const alpha = event.alpha;
+                        const beta = event.beta;
+                        const gamma = event.gamma;
+                        if (alpha != null && beta != null && gamma != null) {
+                            if (!enableShake) {
+                                enableShake = true;
+                                shakeDiv.style.display = "block";
+                                initialOrientation.alpha = alpha;
+                                initialOrientation.beta = beta;
+                                initialOrientation.gamma = gamma;
+                                textDiv.innerText = "试着摇动设备来跳转页面";
+                                shakeDiv.style.height = textDiv.clientHeight + "px";
+                                fullWidth = textDiv.clientWidth;
+                            } else {
+                                if (Hamood.closed) {
+                                    window.removeEventListener('deviceorientation', handleOrientation);
+                                } else {
+                                    let orientationDelta = Math.min(Math.abs(alpha - initialOrientation.alpha), Math.abs(alpha - 360 - initialOrientation.alpha), Math.abs(alpha + 360 - initialOrientation.alpha)) + Math.min(Math.abs(beta - initialOrientation.beta), Math.abs(beta + 360 - initialOrientation.beta), Math.abs(beta - 360 - initialOrientation.beta)) + Math.min(Math.abs(gamma - initialOrientation.gamma), Math.abs(gamma + 180 - initialOrientation.gamma), Math.abs(gamma - 180 - initialOrientation.gamma));
+                                    console.log(orientationDelta);
+                                    if (orientationDelta > 200) {
+                                        window.open(selected.link, "_blank");
+                                        closeHamood();
+                                        orientationDelta = 200;
+                                    };
+                                    shakeDiv.style.width = orientationDelta * fullWidth / 200 + "px";
+                                }
+                            }
+                        }
+                    }
                     textDiv.addEventListener("click", () => {
                         window.open(selected.link, "_blank")
                     });
